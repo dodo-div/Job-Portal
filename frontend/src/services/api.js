326736204662b64b9c12ed884d5e2527516e1f06
dodo-div <divy2324ansh@gmail.com>
2026-04-04 20:03:56 +0530

    Rewrite api.js completely

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,8 @@
 import axios from 'axios';
-
-const BACKEND_URL = 'https://job-portal-backend-8pah.onrender.com/api';
-
-const API = axios.create({
-  baseURL: BACKEND_URL,
-});
-
+const API = axios.create({ baseURL: 'https://job-portal-backend-8pah.onrender.com/api' });
 API.interceptors.request.use((req) => {
   const user = localStorage.getItem('user');
-  if (user) {
-    req.headers.Authorization = `Bearer ${JSON.parse(user).token}`;
-  }
+  if (user) req.headers.Authorization = `Bearer ${JSON.parse(user).token}`;
   return req;
 });
-
 export default API;
